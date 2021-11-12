1051100f669ffc16e6763f1f7ce36e7612ebf875
Nivan Jr <78574377+nivslive@users.noreply.github.com>
2021-11-12 17:11:57 -0300

    Update Home.jsx

--- Sem Auth/frontend/src/components/home/Home.jsx
+++ Sem Auth/frontend/src/components/home/Home.jsx
@@ -6,6 +6,6 @@ import Main from './template/Main'
 export default props =>
 <Main icon="home" title="Início" subtitle="Projeto de React.">
     <div className="display-4">Bem vindo!</div>
-    <p className="mb-0"> Sistema de cadastro desenvolvido em React!</p>
+    <p className="mb-0"> Sistema de cadastro de afazeres desenvolvido em React!</p>
     <hr />
 </Main>
